8bf32c5a8cd512d08eaf5d0ba73e4efdcfeb2ae2
Radek Simko <radek.simko@gmail.com>
2019-07-30 19:17:33 +0100

    WIP

--- httpclient/useragent.go
+++ httpclient/useragent.go
@@ -38,3 +38,93 @@ func (rt *userAgentRoundTripper) RoundTrip(req *http.Request) (*http.Response, e
 	}
 	return rt.inner.RoundTrip(req)
 }
+
+func TerraformUserAgent(version string) *userAgent {
+	return &userAgent{[]*UAProduct{
+		{"HashiCorp", "1.0", ""},
+		{"Terraform", version, ""},
+	}}
+}
+
+type UAProduct struct {
+	Product string
+	Version string
+	Comment string
+}
+
+func (uap *UAProduct) String() string {
+	var b strings.Builder
+	b.WriteString(uap.Product)
+	if uap.Version != "" {
+		b.WriteString(fmt.Sprintf("/%s", uap.Version))
+	}
+	if uap.Comment != "" {
+		b.WriteString(fmt.Sprintf(" (%s)", uap.Comment))
+	}
+	return b.String()
+}
+
+func (uap *UAProduct) Equal(p *UAProduct) bool {
+	if uap.Product == p.Product && uap.Version == p.Version && uap.Comment == p.Comment {
+		return true
+	}
+	return false
+}
+
+type userAgent struct {
+	products []*UAProduct
+}
+
+func (ua *userAgent) Products() []*UAProduct {
+	return ua.products
+}
+
+func (ua *userAgent) String() string {
+	var b strings.Builder
+	for i, p := range ua.products {
+		if i > 0 {
+			b.WriteString(" ")
+		}
+		b.WriteString(p.String())
+	}
+
+	return b.String()
+}
+
+func (ua *userAgent) Append(uap []*UAProduct) *userAgent {
+	ua.products = append(ua.products, uap...)
+	return ua
+}
+
+func (ua *userAgent) Equal(userAgent *userAgent) bool {
+	if len(ua.products) != len(userAgent.products) {
+		return false
+	}
+
+	for i, p := range ua.products {
+		if p != userAgent.products[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func UserAgent(products []*UAProduct) *userAgent {
+	ua := &userAgent{products}
+
+	if add := os.Getenv(uaEnvVar); add != "" {
+		add = strings.TrimSpace(add)
+		if len(add) > 0 {
+			ua.Append(ParseUserAgentString(add))
+			log.Printf("[DEBUG] Using modified User-Agent: %s", ua)
+		}
+	}
+
+	return ua
+}
+
+func ParseUserAgentString(uaString string) []*UAProduct {
+	// TODO
+	return []*UAProduct{}
+}
--- httpclient/useragent_test.go
+++ httpclient/useragent_test.go
@@ -43,5 +43,73 @@ func TestUserAgentString_env(t *testing.T) {
 			}
 		})
 	}
+}
 
+func TestParseUserAgentString(t *testing.T) {
+	testCases := []struct {
+		uaString   string
+		uaProducts []*UAProduct
+	}{
+		{
+			"terraform-github-actions/1.0",
+			[]*UAProduct{{"terraform-github-actions", "1.0", ""}},
+		},
+		{
+			"TFE/a718e58f",
+			[]*UAProduct{{"TFE", "a718e58f", ""}},
+		},
+		{
+			"OneProduct/0.1.0 AnotherOne/1.2",
+			[]*UAProduct{{"OneProduct", "0.1.0", ""}, {"AnotherOne", "1.2", ""}},
+		},
+		{
+			"ProductWithComment/1.0.0 (a comment; goes; here)",
+			[]*UAProduct{{"ProductWithComment", "1.0.0", "a comment; goes; here"}},
+		},
+		{
+			"ProductWithComment/1.0.0 (a comment; goes; here) AnotherProductWithComment/5.5.0 (blah)",
+			[]*UAProduct{
+				{"ProductWithComment", "1.0.0", "a comment; goes; here"},
+				{"AnotherProductWithComment", "5.5.0", "blah"},
+			},
+		},
+		{
+			"NoComment/1.0.0 AnotherProductWithComment/5.5.0 (blah)",
+			[]*UAProduct{
+				{"NoComment", "1.0.0", ""},
+				{"AnotherProductWithComment", "5.5.0", "blah"},
+			},
+		},
+		{
+			"First/1.0.0 Second/5.5.0 Third/5.5.0",
+			[]*UAProduct{
+				{"First", "1.0.0", ""},
+				{"Second", "5.5.0", ""},
+				{"Third", "5.5.0", ""},
+			},
+		},
+	}
+	for i, tc := range testCases {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			givenUA := UserAgent(ParseUserAgentString(tc.uaString))
+			expectedUA := UserAgent(tc.uaProducts)
+
+			if !givenUA.Equal(expectedUA) {
+				t.Fatalf("Unexpected User-Agent.\nExpected: %q\nGiven: %q\n", expectedUA, givenUA)
+			}
+		})
+	}
+
+}
+
+func TestUserAgentAppendViaEnvVar(t *testing.T) {
+	t.Fatal("Not implemented")
+}
+
+func TestUserAgentString(t *testing.T) {
+	t.Fatal("Not implemented")
+}
+
+func TestUserAgentAppend(t *testing.T) {
+	t.Fatal("Not implemented")
 }
